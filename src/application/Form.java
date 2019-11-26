b3534820eb146ceb47ca3fbd9a023f139acf34dd
Michael Shepherd <shepherdm1@wit.edu>
2019-11-26 13:44:32 -0500

    finishing controller etc

--- src/application/Form.java
+++ src/application/Form.java
@@ -26,32 +26,41 @@ public class Form {
 		this.c=c;
 		this.d=d;
 		this.name = name;
+		switch (name) {
+			case("j"):
+				color = Color.SLATEGRAY;
+				break;
+			case "l":
+				color = Color.DARKGOLDENROD;
+				break;
+			case "o":
+				color = Color.INDIANRED;
+				break;
+			case "s":
+				color = Color.FORESTGREEN;
+				break;
+			case "t":
+				color = Color.CADETBLUE;
+				break;
+			case "z":
+				color = Color.HOTPINK;
+				break;
+			case "i":
+				color = Color.SANDYBROWN;
+				break;		
+		}
+		this.a.setFill(color);
+		this.b.setFill(color);
+		this.c.setFill(color);
+		this.d.setFill(color);
 	}
 	
-	switch (name) {
-		case "j":
-			color = Color.SLATEGRAY;
-			break;
-		case "l":
-			color = Color.DARKGOLDENROD;
-			break;
-		case "o":
-			color = Color.INDIANRED;
-			break;
-		case "s":
-			color = Color.FORESTGREEN;
-			break;
-		case "t":
-			color = Color.CADETBLUE;
-			break;
-		case "z":
-			color = Color.HOTPINK;
-			break;
-		case "i":
-			color = Color.SANDYBROWN;
-			break;
-			
+	public String getName() {
+		return name;
 	}
 	
-
+	public void changeForm() {
+		if(form!=4) form++;
+		else form = 1;
+	}
 }
